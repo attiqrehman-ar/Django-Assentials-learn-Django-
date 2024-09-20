b9c61e17393bfecffc2b3449ce1e73739710814e
attiqrehman-ar <attiqrehman130@gmail.com>
2024-09-20 14:01:29 +0500

    add models and connect it with admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from . import models
+
+
+class MyNotesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.MyNotes, MyNotesAdmin)
