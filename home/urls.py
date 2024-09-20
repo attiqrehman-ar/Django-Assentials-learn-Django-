6cf4ff6333520bacf5989481d35550db53d23dfa
attiqrehman-ar <attiqrehman130@gmail.com>
2024-09-21 02:20:22 +0500

    convert home's function views into class based views

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 
 urlpatterns = [
-    path("home/", views.home),
-    path("authorized/", views.authorized)
+    path("home/", views.HomeView.as_view()),
+    path("authorized/", views.AuthorizedView.as_view())
 ]
