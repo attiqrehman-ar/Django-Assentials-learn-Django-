273942fdd5cff957db5431166c0bf199997bda79
attiqrehman-ar <attiqrehman130@gmail.com>
2024-09-22 17:27:57 +0500

    setup base html for django templates

--- LearnDjango/settings.py
+++ LearnDjango/settings.py
@@ -59,7 +59,10 @@ ROOT_URLCONF = 'LearnDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+
+            BASE_DIR /'static/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS=[
+    BASE_DIR /'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
