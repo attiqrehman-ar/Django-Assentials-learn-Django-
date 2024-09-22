a688e547a86594b92b68383b9c6581ae27a8e03e
attiqrehman-ar <attiqrehman130@gmail.com>
2024-09-23 01:21:46 +0500

    create Update function in notes app

--- notes/urls.py
+++ notes/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
    path('notes', views.NotesListView.as_view(), name="notes.list"),
    path('notes/<int:pk>', views.NOtesDetailView.as_view(), name="note.detail"),
-   path("notes/new", views.NotesCreateView.as_view(), name='notes.new')
+   path("notes/new", views.NotesCreateView.as_view(), name='notes.new'),
+   path("notes/<int:pk>/edit", views.NotesUpdateView.as_view(), name='notes.update')
 ]
--- notes/views.py
+++ notes/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from notes.forms import NotesForm
 from notes.models import Notes
 from django.http import Http404
-from django.views.generic import ListView,DetailView, CreateView
+from django.views.generic import ListView,DetailView, CreateView, UpdateView
 # Create your views here.
 
 class NotesCreateView(CreateView):
     model=Notes
     form_class=NotesForm
     success_url="/smart/notes"
+
+
+class NotesUpdateView(UpdateView):
+      model=Notes
+      form_class=NotesForm
+      success_url="/smart/notes"
 
 class NotesListView(ListView):
     model=Notes
