0fef7543729459c3d7620a4dbcbbe4ccdd6bb547
attiqrehman-ar <attiqrehman130@gmail.com>
2024-09-23 02:17:51 +0500

    setup foreign key and authentications

--- notes/models.py
+++ notes/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Notes(models.Model):
     title_Name= models.CharField(max_length=200)
     textt= models.TextField()
     description=models.TextField()
-    
-
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,13 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from notes.forms import NotesForm
 from notes.models import Notes
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.views.generic import ListView,DetailView, CreateView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
 
@@ -17,15 +21,26 @@ class NotesCreateView(CreateView):
     form_class=NotesForm
     success_url="/smart/notes"
 
+    def form_valid(self, form):
+       self.object=form.save(commit=False)
+       self.object.user= self.request.user
+       self.object.save()
+       return HttpResponseRedirect(self.get_success_url())
+    
+
 
 class NotesUpdateView(UpdateView):
       model=Notes
       form_class=NotesForm
       success_url="/smart/notes"
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model=Notes
     context_object_name="notes"
+    login_url="/admin"
+
+    def get_queryset(self):
+         return self.request.user.notes.all()
 
 
 
