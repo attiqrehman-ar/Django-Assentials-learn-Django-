d828f6b024657960fcc01b32f6b7888ccf41dc7f
attiqrehman-ar <attiqrehman130@gmail.com>
2024-09-24 01:17:30 +0500

    add login logout authentications

--- home/views.py
+++ home/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class LogoutInterfaceView(LogoutView):
+    template_name="home/logout_home.html"
+    
+
+class LoginInterfaceView(LoginView):
+    template_name="home/login.html"
+
 
 class HomeView(TemplateView):
     template_name="home/welcome.html"
