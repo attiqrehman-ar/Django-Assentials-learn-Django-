92c66f2b82fdb87bf0f92388c8b924319e8580b4
attiqrehman-ar <attiqrehman130@gmail.com>
2024-09-24 02:08:33 +0500

    add signup functionality

--- LearnDjango/settings.py
+++ LearnDjango/settings.py
@@ -134,3 +134,4 @@ STATICFILES_DIRS=[
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL="/smart/notes"
+# LOGOUT_REDIRECT_URL="/logout/"
--- home/views.py
+++ home/views.py
@@ -4,10 +4,22 @@ from datetime import datetime
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
+
+class SignupView(CreateView):
+    form_class=UserCreationForm
+    template_name='home/register.html'
+    success_url='smart/notes'
 
 
-class LogoutInterfaceView(LogoutView):
-    template_name="home/logout_home.html"
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('notes.list')
+
+# class LogoutInterfaceView(LogoutView):
+#     template_name="home/logout_home.html"
     
 
 class LoginInterfaceView(LoginView):
